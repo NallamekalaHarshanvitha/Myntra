150e8c317aa8be5310817268f7a2e8244cf2f1b0
Nallamekala.Harshanvitha <Nallamekala.harshanvitha@accionlabs.com>
2026-08-05 16:49:22 +0530

    Added the search elements and modified in both files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
- 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
+import { useSearchQuery } from "./hooks/useSearch";
 
 function App() {
-  const [search, setSearch] = useState("");
- 
+  const { search, setSearch, clearSearch } = useSearchQuery("");
+
   return (
     <>
-      <Navbar search={search} setSearch={setSearch} />
- 
+      <Navbar search={search} setSearch={setSearch} clearSearch={clearSearch} />
+
       <Routes>
-       <Route path="/" element={<Home search={search} />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/" element={<Home search={search} />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
     </>
   );
 }
- 
+
 export default App;
