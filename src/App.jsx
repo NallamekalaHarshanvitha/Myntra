0e594014b9a8200b7f6b604a16eb14057af03475
Nallamekala.Harshanvitha <Nallamekala.harshanvitha@accionlabs.com>
2026-08-06 13:12:11 +0530

    Change in import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { useSearchQuery } from "./hooks/useSearch";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
-import { useSearchQuery } from "./hooks/useSearch";
 
 function App() {
  
